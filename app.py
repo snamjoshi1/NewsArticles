910b70fddc49814f81bb27d1422d09084cce7c8f
snamjoshi1 <40161254+snamjoshi1@users.noreply.github.com>
2023-08-18 05:49:58 +0000

    Created a dynamic HTML Page

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+News=[
+  {
+    'link':'economictimes.com',
+    'headline':'360 One raises funds',
+    'description':'The management raised 4B$',
+    'date': 'Aug 18 2023'
+  },
+   {
+    'link':'livemint.com',
+    'headline':'IIFL One raises funds',
+    'description':'The management raised 4B$',
+    'date': 'Aug 17 2023'
+  },
+]
 
 @app.route("/")
 def hello_world():
-  return "Hello World"
+  return render_template('home.html',jobs=News)
 
 
 if __name__ == "__main__":
